3696cf7a1d545c2714b3acee7dc8d014ee97e01d
Vincent <vincent.j.wilson317@gmail.com>
2023-06-14 17:27:56 -0500

    fixed matching view file

--- d-app/metro.config.js
+++ d-app/metro.config.js
@@ -1,7 +1,7 @@
 const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
-
+        
 /** @type {import('expo/metro-config').MetroConfig} */
 const { resolver: { sourceExts } } = config;
 
